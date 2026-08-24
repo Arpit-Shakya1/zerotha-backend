06b43e3a6d81135bf7bf5b875f215fe6fa940e41
Arpit <arpitshakya1372007@gmail.com>
2026-08-24 13:55:04 +0530

    COmplete Routing

--- index.js
+++ index.js
@@ -3,63 +3,43 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
-
+ const {OrdersModel} = require('./model/OrdersModel')
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
+app.use(cors());
+app.use(bodyParser.json());
 
-let tempPositions = [
-    {
-        product: "CNC",
-        name: "EVEREADY",
-        qty: 2,
-        avg: 316.27,
-        price: 312.35,
-        net: "+0.58%",
-        day: "-1.24%",
-        isLoss: true,
-    },
-    {
-        product: "CNC",
-        name: "JUBLFOOD",
-        qty: 1,
-        avg: 3124.75,
-        price: 3082.65,
-        net: "+10.04%",
-        day: "-1.35%",
-        isLoss: true,
-    },
-];
 
-app.get("/addPositions", async (req, res) => {
-    try {
-        for (const item of tempPositions) {
-            const newPosition = new PositionsModel({
-                product: item.product,
-                name: item.name,
-                qty: item.qty,
-                avg: item.avg,
-                price: item.price,
-                net: item.net,
-                day: item.day,
-                isLoss: item.isLoss || false,
-            });
-
-            await newPosition.save();
-        }
-
-        res.send("Positions Added Successfully!");
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error while saving positions.");
-    }
+app.get('/allHoldings', async(req,res)=>{
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings)
 });
+
+app.get("/allPositions", async(req,res) =>{
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+});
+
+app.post('/newOrder',(req, res)=>{
+    let newOrder = new OrdersModel({
+        name:req.body.name,
+        qty:req.body.qty,
+        price:req.body.price,
+        mode:req.body.mode,
+    });
+
+    newOrder.save();
+    res.send("order saved");
+})
 
 mongoose
     .connect(uri)
