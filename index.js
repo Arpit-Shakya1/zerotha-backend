7df1a8342579f349499307cc47dff6ed8739801f
Arpit <arpitshakya1372007@gmail.com>
2026-09-17 10:21:38 +0530

    dadabase atlas

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
 require("dotenv").config();
+
+const dns = require("dns");
+
+dns.setServers([
+  "8.8.8.8",
+  "1.1.1.1"
+]);
+
+
 const jwt = require("jsonwebtoken");
 const { UserModel } = require("./model/UserModel");
 
