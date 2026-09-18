12331dc6ba13c40f8595649284397cec5672cac7
Arpit <arpitshakya1372007@gmail.com>
2026-09-18 11:59:37 +0530

    Server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 require("dotenv").config();
 
+// const dns = require("dns");
+
+// dns.setServers([
+//   "8.8.8.8",
+//   "1.1.1.1"
+// ]);
+
+
 const dns = require("dns");
 
 dns.setServers([
-  "8.8.8.8",
-  "1.1.1.1"
+  "10.67.83.66",
 ]);
 
 
